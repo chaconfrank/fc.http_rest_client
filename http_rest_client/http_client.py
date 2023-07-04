95b53f003dbc0250f85282f2dec8c588bd9b289a
chaconfrank <fchacon@webs314.com>
2023-07-04 22:41:02 +0200

    add retries with catch timeout exception

--- http_rest_client/http_client.py
+++ http_rest_client/http_client.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 import requests
 from requests import Response
+from requests.exceptions import Timeout
 
 
 class HttpMethods(Enum):
@@ -12,30 +13,42 @@ class HttpMethods(Enum):
 
 
 class HttpClient:
-    def __init__(self, base_url):
+    def __init__(self, base_url, max_tries=1, time_out=100):
+        self.tries: int = 0
         self.base_url = base_url
         self.headers = {"Content-Type": "application/json"}
+        self.max_tries = max_tries
+        self.time_out = time_out/1000
 
     @property
     def get_headers(self):
         return self.headers
 
-    def send_request(self, http_method: HttpMethods, endpoint: str, data=None) -> Response:
+    @property
+    def get_tries(self):
+        return self.tries
+
+    def send_request(
+            self, http_method: HttpMethods, endpoint: str, data=None) -> Response | TimeoutError | ValueError:
+
         url = self.base_url + endpoint
 
-        if http_method == HttpMethods.GET:
-            response = requests.get(url, headers=self.get_headers)
-        elif http_method == HttpMethods.POST:
-            response = requests.post(url, headers=self.get_headers, json=data)
-        elif http_method == HttpMethods.PUT:
-            response = requests.put(url, headers=self.get_headers, json=data)
-        elif http_method == HttpMethods.DELETE:
-            response = requests.delete(url, headers=self.get_headers)
-        else:
-            raise ValueError("Invalid HTTP method")
+        self.tries = 0
+        while self.tries < self.max_tries:
+            try:
+                if http_method == HttpMethods.GET:
+                    return requests.get(url, headers=self.get_headers, timeout=self.time_out)
+                elif http_method == HttpMethods.POST:
+                    return requests.post(url, headers=self.get_headers, json=data, timeout=self.time_out)
+                elif http_method == HttpMethods.PUT:
+                    return requests.put(url, headers=self.get_headers, json=data, timeout=self.time_out)
+                elif http_method == HttpMethods.DELETE:
+                    return requests.delete(url, headers=self.get_headers, timeout=self.time_out)
+                else:
+                    raise ValueError("Invalid HTTP method")
+            except Timeout:
+                print(f"Timeout occurred. Retrying ({self.tries + 1}/{self.max_tries})...")
+            finally:
+                self.tries += 1
 
-        return response
-
-
-
-
+        raise Timeout("Maximum number of retries exceeded.")
--- tests/test_http_client.py
+++ tests/test_http_client.py
@@ -1,6 +1,7 @@
 import unittest
 import requests_mock
 
+from requests.exceptions import Timeout
 from http_rest_client.http_client import HttpClient, HttpMethods
 
 
@@ -13,3 +14,13 @@ class HttpClientTest(unittest.TestCase):
             response = client.send_request(HttpMethods.GET, '/users')
 
             self.assertEqual(response.status_code, 200)
+
+    def test_get_request_with_time_out(self):
+        with requests_mock.Mocker() as mocker:
+            mocker.get('https://api.example.com/users', exc=Timeout)
+
+            with self.assertRaises(Timeout):
+                client = HttpClient('https://api.example.com', max_tries=2, time_out=500)
+                client.send_request(HttpMethods.GET, '/users')
+
+            self.assertEqual(client.get_tries, 2)
